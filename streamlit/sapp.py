9e137ef82fdf738417fd8b0250d56ec5b6a04f5c
Yousef Barakat <yousefhany19992@gmail.com>
2023-02-03 00:53:41 +0100

    Update sapp.py

--- streamlit/sapp.py
+++ streamlit/sapp.py
@@ -143,7 +143,7 @@ def complete():
     import streamlit as st
 
     st.header('Interactive dashboard')
-    minp, maxp = st.select_slider('''Most importantly, what's your price range?''', df['Price (HUF)'].sort_values(), (df['Price (HUF)'].min(), df['Price (HUF)'].max()), key='price_select')
+    minp, maxp = st.select_slider('''Most importantly, what's your price range? (in HUF)''', df['Price (HUF)'].sort_values(), (df['Price (HUF)'].min(), df['Price (HUF)'].max()), key='price_select')
     rentee = st.radio('Are you moving in alone or with others?', ('Alone', 'With others'), key='choice')
     if rentee == 'With others':
         num = st.number_input('How many others?', 1, 4, key='people')
