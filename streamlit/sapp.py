7bc6d01fdaee7024fd110671146079655c5f8594
Yousef Barakat <yousefhany19992@gmail.com>
2023-02-03 00:54:57 +0100

    Update sapp.py

--- streamlit/sapp.py
+++ streamlit/sapp.py
@@ -161,7 +161,9 @@ def complete():
     else:
         st.success(f'There are a total of {df1["Price (HUF)"].count()} properties that match your description!')
         st.dataframe(df1)
-
+    if st.button('Reset'):
+        st.experimental_rerun()
+        
 page_names_to_funcs = {
     "—": intro,
     "General info": general,
