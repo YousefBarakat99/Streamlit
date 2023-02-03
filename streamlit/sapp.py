177482de339183bde9f7b9376ab6979c7a35f0d7
Yousef Barakat <yousefhany19992@gmail.com>
2023-02-03 23:09:14 +0100

    Update sapp.py

--- streamlit/sapp.py
+++ streamlit/sapp.py
@@ -13,7 +13,7 @@ def intro():
 
     st.markdown(
         """
-        ### Some Context
+        ### Some Context:
 
         This App explores the relation between price, size and number of rooms along a relatively large dataset. The data was scraped from 2 property websites in the area that I live in. The motivation behind this project was
         to try and find a solution to easily having a large excel file with properties and filtering them while having an easily accessable link that leads directly to the posting after hearing a friend who was complaining about finding a 
