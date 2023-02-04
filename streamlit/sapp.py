51d324f0ed9de767c865a4cb239e9e8116108766
Yousef Barakat <yousefhany19992@gmail.com>
2023-02-04 23:29:32 +0100

    Update sapp.py

--- streamlit/sapp.py
+++ streamlit/sapp.py
@@ -147,7 +147,7 @@ def complete():
     minp, maxp = st.select_slider('''Most importantly, what's your price range? (in HUF)''', df['Price (HUF)'].sort_values(), (df['Price (HUF)'].min(), df['Price (HUF)'].max()), key='price_select')
     rentee = st.radio('Are you moving in alone or with others?', ('Alone', 'With others'), key='choice')
     if rentee == 'With others':
-        num = st.number_input('How many others?', 1, 4, key='people') + 1
+        num = st.number_input('How many others?', 1, df['Rooms'].max() - 1, key='people') + 1
     else:
         num = 1
     
