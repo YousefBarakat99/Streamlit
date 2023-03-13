3ee367bc3ceadf8f0a33a623ad5212d46fb1c0db
Yousef Barakat <yousefhany19992@gmail.com>
2023-03-13 21:50:40 +0100

    up to date

--- streamlit/Streamlit_App.py
+++ streamlit/Streamlit_App.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-#* Sending data to streamlit
+# * Sending data to streamlit
 df = pd.read_excel('streamlit/Houses_Cleaned.xlsx')
 # df = pd.read_excel('Houses_Cleaned.xlsx')
 df['Address'] = df['Address'].fillna('Address unavailable')
-def create_link(url:str) -> str:
+
+
+def create_link(url: str) -> str:
     return f'''<a href="{url}">🔗</a>'''
+
+
 df['Link'] = [create_link(url) for url in df["Link"]]
 
-today = '2023-03-10'
+today = '2023-03-13'
+
 
 def intro():
     import streamlit as st
@@ -50,8 +55,8 @@ def intro():
         [<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2560px-LinkedIn_Logo.svg.png" width="150"/>](https://www.linkedin.com/in/yousef-barakat-019816205/)
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[<img src="https://cdn-icons-png.flaticon.com/512/3037/3037366.png" height="40"/>](https://yousefbarakat99.github.io/website/)
         
-    """
-    , unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
+
 
 def general():
     import streamlit as st
@@ -64,15 +69,15 @@ def general():
     # st.markdown(df.to_html(render_links=True),unsafe_allow_html=True)
 
     fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df.columns.to_list()],
-                fill_color='black'
+        data=[
+            go.Table(
+                columnwidth=[1, 1, 0.5],
+                header=dict(
+                    values=[f"<b>{i}</b>" for i in df.columns.to_list()],
+                    fill_color='black'
                 ),
-            cells=dict(
-                values=df.transpose()
+                cells=dict(
+                    values=df.transpose()
                 )
             )
         ]
@@ -96,7 +101,7 @@ def general():
         # ax.set_xlabel('Number of rooms')
         # ax.set_title('Count of properties with number of rooms')
         fig = px.histogram(df, x='Rooms', color='Rooms')
-        fig.update_traces(marker_line_width=2,marker_line_color="black")
+        fig.update_traces(marker_line_width=2, marker_line_color="black")
         rooms_dict = list(df['Rooms'].value_counts().to_dict())
         st.write(f'''The most common number of rooms amongst properties is {rooms_dict[0]}, followed by {rooms_dict[1]} then by {rooms_dict[2]}. 
         This can be due to the fact that not all websites or agents count rooms in the same way.
@@ -116,32 +121,36 @@ def general():
         fig1 = sns.pairplot(df)
         st.pyplot(fig1)
 
+
 def rooms_ft():
     import streamlit as st
     import plotly.express as px
-    
+
     st.write('Filter by rooms')
     rooms = st.radio('Number of rooms', ('1', '2', '3', '>3'))
     if rooms == '1':
         # st.dataframe(df[df['Rooms'] == 2].sort_values('Price (HUF)').drop(columns='Address').reset_index(drop=True))
-        df1 = df[(df['Rooms'] == 1) & (df['Price (HUF)'] > 50_000) & (df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
+        df1 = df[(df['Rooms'] == 1) & (df['Price (HUF)'] > 50_000) & (
+            df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
         fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
-                fill_color='black'
-                ),
-            cells=dict(
-                values=df1.transpose()
+            data=[
+                go.Table(
+                    columnwidth=[1, 1, 0.5],
+                    header=dict(
+                        values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
+                        fill_color='black'
+                    ),
+                    cells=dict(
+                        values=df1.transpose()
+                    )
                 )
-            )
-        ]
-    )
+            ]
+        )
         st.plotly_chart(fig, use_container_width=True)
-        st.write(f'There are {len(df1)} properties with just one room and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
-        fig = px.line(df1, x='Price (HUF)', y='Size (m2)', title='Price change according to Size')
+        st.write(
+            f'There are {len(df1)} properties with just one room and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
+        fig = px.line(df1, x='Price (HUF)', y='Size (m2)',
+                      title='Price change according to Size')
         st.plotly_chart(fig, use_container_width=True)
         # fig, ax = plt.subplots()
         # ax.plot('Price (HUF)', 'Size (m2)', data=df1)
@@ -151,24 +160,27 @@ def rooms_ft():
         # st.pyplot(fig)
     elif rooms == '2':
         # st.dataframe(df[df['Rooms'] == 2].sort_values('Price (HUF)').drop(columns='Address').reset_index(drop=True))
-        df1 = df[(df['Rooms'] == 2) & (df['Price (HUF)'] > 40_000) & (df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
+        df1 = df[(df['Rooms'] == 2) & (df['Price (HUF)'] > 40_000) & (
+            df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
         fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
-                fill_color='black'
-                ),
-            cells=dict(
-                values=df1.transpose()
+            data=[
+                go.Table(
+                    columnwidth=[1, 1, 0.5],
+                    header=dict(
+                        values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
+                        fill_color='black'
+                    ),
+                    cells=dict(
+                        values=df1.transpose()
+                    )
                 )
-            )
-        ]
-    )
+            ]
+        )
         st.plotly_chart(fig, use_container_width=True)
-        st.write(f'There are {len(df1)} properties with two rooms and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
-        fig = px.line(df1, x='Price (HUF)', y='Size (m2)', title='Price change according to Size')
+        st.write(
+            f'There are {len(df1)} properties with two rooms and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
+        fig = px.line(df1, x='Price (HUF)', y='Size (m2)',
+                      title='Price change according to Size')
         st.plotly_chart(fig, use_container_width=True)
         # fig, ax = plt.subplots()
         # ax.plot('Price (HUF)', 'Size (m2)', data=df1)
@@ -178,24 +190,27 @@ def rooms_ft():
         # st.pyplot(fig)
     elif rooms == '3':
         # st.dataframe(df[df['Rooms'] == 3].sort_values('Price (HUF)').drop(columns='Address').reset_index(drop=True))
-        df1 = df[(df['Rooms'] == 3) & (df['Price (HUF)'] > 40_000) & (df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
+        df1 = df[(df['Rooms'] == 3) & (df['Price (HUF)'] > 40_000) & (
+            df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
         fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
-                fill_color='black'
-                ),
-            cells=dict(
-                values=df1.transpose()
+            data=[
+                go.Table(
+                    columnwidth=[1, 1, 0.5],
+                    header=dict(
+                        values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
+                        fill_color='black'
+                    ),
+                    cells=dict(
+                        values=df1.transpose()
+                    )
                 )
-            )
-        ]
-    )
+            ]
+        )
         st.plotly_chart(fig, use_container_width=True)
-        st.write(f'There are {len(df1)} properties with three rooms and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
-        fig = px.line(df1, x='Price (HUF)', y='Size (m2)', title='Price change according to Size')
+        st.write(
+            f'There are {len(df1)} properties with three rooms and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
+        fig = px.line(df1, x='Price (HUF)', y='Size (m2)',
+                      title='Price change according to Size')
         st.plotly_chart(fig, use_container_width=True)
         # fig, ax = plt.subplots()
         # ax.plot('Price (HUF)', 'Size (m2)', data=df1)
@@ -205,24 +220,27 @@ def rooms_ft():
         # st.pyplot(fig)
     elif rooms == '>3':
         # st.dataframe(df[df['Rooms'] >3 ].sort_values('Price (HUF)').drop(columns='Address').reset_index(drop=True))
-        df1 = df[(df['Rooms'] > 3) & (df['Price (HUF)'] > 40_000) & (df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
+        df1 = df[(df['Rooms'] > 3) & (df['Price (HUF)'] > 40_000) & (
+            df['Size (m2)'].notna())].sort_values('Price (HUF)').reset_index(drop=True)
         fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
-                fill_color='black'
-                ),
-            cells=dict(
-                values=df1.transpose()
+            data=[
+                go.Table(
+                    columnwidth=[1, 1, 0.5],
+                    header=dict(
+                        values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
+                        fill_color='black'
+                    ),
+                    cells=dict(
+                        values=df1.transpose()
+                    )
                 )
-            )
-        ]
-    )
+            ]
+        )
         st.plotly_chart(fig, use_container_width=True)
-        st.write(f'There are {len(df1)} properties with more than three rooms and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
-        fig = px.line(df1, x='Price (HUF)', y='Size (m2)', title='Price change according to Size')
+        st.write(
+            f'There are {len(df1)} properties with more than three rooms and the average price is {int(df1["Price (HUF)"].mean())} HUF.')
+        fig = px.line(df1, x='Price (HUF)', y='Size (m2)',
+                      title='Price change according to Size')
         st.plotly_chart(fig, use_container_width=True)
         # fig, ax = plt.subplots()
         # ax.plot('Price (HUF)', 'Size (m2)', data=df1)
@@ -232,6 +250,7 @@ def rooms_ft():
         # ax.ticklabel_format(style='plain')
         # st.pyplot(fig)
 
+
 def price_ft():
     import streamlit as st
     import plotly.express as px
@@ -239,8 +258,9 @@ def price_ft():
     st.header('Filter by prices using a slider')
     st.write()
     st.write('Illustration of most common prices')
-    fig = px.histogram(df, x='Price (HUF)', color_discrete_sequence=['indianred'])
-    fig.update_traces(marker_line_width=2,marker_line_color="black")
+    fig = px.histogram(df, x='Price (HUF)',
+                       color_discrete_sequence=['indianred'])
+    fig.update_traces(marker_line_width=2, marker_line_color="black")
     st.plotly_chart(fig, use_container_width=True)
     # fig, ax = plt.subplots()
     # ax.hist('Price (HUF)', data=df, edgecolor='black')
@@ -248,27 +268,30 @@ def price_ft():
     # ax.set_title('Count of properties according to price')
     # ax.ticklabel_format(style='plain')
     # st.pyplot(fig)
-    minp, maxp = st.select_slider('Select price range', df['Price (HUF)'].sort_values(), (df['Price (HUF)'].min(), df['Price (HUF)'].max()))
-    df1 = df[(df['Price (HUF)'] <= maxp) & (df['Price (HUF)'] >= minp)].sort_values('Price (HUF)').drop(columns='Address').reset_index(drop=True)
+    minp, maxp = st.select_slider('Select price range', df['Price (HUF)'].sort_values(
+    ), (df['Price (HUF)'].min(), df['Price (HUF)'].max()))
+    df1 = df[(df['Price (HUF)'] <= maxp) & (df['Price (HUF)'] >= minp)].sort_values(
+        'Price (HUF)').drop(columns='Address').reset_index(drop=True)
     st.write(f'''There are a total of {df1["Price (HUF)"].count()} properties within that price range. 
     The average size is {df1["Size (m2)"].mean().round(1)} m2 whereas the average number of rooms 
     is {df1["Rooms"].mean().round(1)}''')
     # st.dataframe(df1)
     fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
-                fill_color='black'
+        data=[
+            go.Table(
+                columnwidth=[1, 1, 0.5],
+                header=dict(
+                    values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
+                    fill_color='black'
                 ),
-            cells=dict(
-                values=df1.transpose()
+                cells=dict(
+                    values=df1.transpose()
                 )
             )
         ]
     )
     st.plotly_chart(fig, use_container_width=True)
+
 
 def complete():
     import streamlit as st
@@ -276,47 +299,57 @@ def complete():
 
     st.header('Interactive dashboard')
     st.write(f'Data updated on: {today}')
-    minp, maxp = st.select_slider('''Most importantly, what's your price range?''', df['Price (HUF)'].sort_values(), (df['Price (HUF)'].min(), df['Price (HUF)'].max()), key='price_select')
-    rentee = st.radio('Are you moving in alone or with others?', ('Alone', 'With others'), key='choice')
+    minp, maxp = st.select_slider('''Most importantly, what's your price range?''', df['Price (HUF)'].sort_values(
+    ), (df['Price (HUF)'].min(), df['Price (HUF)'].max()), key='price_select')
+    rentee = st.radio('Are you moving in alone or with others?',
+                      ('Alone', 'With others'), key='choice')
     if rentee == 'With others':
         num = st.number_input('How many others?', 1, 4, key='people') + 1
     else:
         num = 1
-    
-    sizes = st.radio('''Any size in mind?''', ('''Doesn't matter''', '''I have a size in mind'''), key='s_choice')
+
+    sizes = st.radio('''Any size in mind?''', ('''Doesn't matter''',
+                     '''I have a size in mind'''), key='s_choice')
     if sizes == 'I have a size in mind':
-        mins, maxs = st.select_slider('''Select size range''', df['Size (m2)'].dropna().sort_values(), (df['Size (m2)'].min(), df['Size (m2)'].max()), key='size')
+        mins, maxs = st.select_slider('''Select size range''', df['Size (m2)'].dropna(
+        ).sort_values(), (df['Size (m2)'].min(), df['Size (m2)'].max()), key='size')
     else:
-        mins, maxs = df['Size (m2)'].dropna().min(), df['Size (m2)'].dropna().max()
-    df1 = df[(df['Rooms'] <= (num+1)) & (df['Price (HUF)'] <= maxp) & (df['Price (HUF)'] >= minp) & (df['Size (m2)'] <= maxs) & ((df['Size (m2)'] >= mins))].sort_values('Price (HUF)').reset_index(drop=True)
+        mins, maxs = df['Size (m2)'].dropna(
+        ).min(), df['Size (m2)'].dropna().max()
+    df1 = df[(df['Rooms'] <= (num+1)) & (df['Price (HUF)'] <= maxp) & (df['Price (HUF)'] >= minp) &
+             (df['Size (m2)'] <= maxs) & ((df['Size (m2)'] >= mins))].sort_values('Price (HUF)').reset_index(drop=True)
     if len(df1) == 0:
         st.error('There no properties that match your description.')
     else:
-        st.success(f'There are a total of {df1["Price (HUF)"].count()} properties that match your description!')
+        st.success(
+            f'There are a total of {df1["Price (HUF)"].count()} properties that match your description!')
         # st.dataframe(df1)
         fig = go.Figure(
-    data=[
-        go.Table(
-            columnwidth = [1,1,0.5],
-            header=dict(
-                values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
-                fill_color='black'
-                ),
-            cells=dict(
-                values=df1.transpose()
+            data=[
+                go.Table(
+                    columnwidth=[1, 1, 0.5],
+                    header=dict(
+                        values=[f"<b>{i}</b>" for i in df1.columns.to_list()],
+                        fill_color='black'
+                    ),
+                    cells=dict(
+                        values=df1.transpose()
+                    )
                 )
-            )
-        ]
-    )
+            ]
+        )
         st.plotly_chart(fig, use_container_width=True)
         fig = px.histogram(df1, x='Rooms', color='Rooms')
-        fig.update_traces(marker_line_width=2,marker_line_color="black")
+        fig.update_traces(marker_line_width=2, marker_line_color="black")
         st.plotly_chart(fig, use_container_width=True)
-        fig = px.histogram(df1, x='Price (HUF)', color_discrete_sequence=['turquoise'])
-        fig.update_traces(marker_line_width=2,marker_line_color="black")
+        fig = px.histogram(df1, x='Price (HUF)',
+                           color_discrete_sequence=['turquoise'])
+        fig.update_traces(marker_line_width=2, marker_line_color="black")
         st.plotly_chart(fig, use_container_width=True)
-        fig1 = px.line(df1, x='Price (HUF)', y='Size (m2)', title='Price change according to Size')
+        fig1 = px.line(df1, x='Price (HUF)', y='Size (m2)',
+                       title='Price change according to Size')
         st.plotly_chart(fig1, use_container_width=True)
+
 
 page_names_to_funcs = {
     "—": intro,
@@ -328,4 +361,3 @@ page_names_to_funcs = {
 
 demo_name = st.sidebar.selectbox("Choose a page", page_names_to_funcs.keys())
 page_names_to_funcs[demo_name]()
-
--- streamlit/scraping.py
+++ streamlit/scraping.py
@@ -11,56 +11,62 @@ import re
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
-driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe", chrome_options=options)
+driver = webdriver.Chrome(
+    "C:\Program Files (x86)\chromedriver.exe", chrome_options=options)
 driver.get('https://www.findahome.hu/ingatlanok/?search=all')
 
 page = driver.page_source
 soup = BeautifulSoup(page, 'html.parser')
-posts = soup.find_all('div', class_='ingatlan-cont grid-23 mobile-grid-100 tablet-grid-23')
+posts = soup.find_all(
+    'div', class_='ingatlan-cont grid-23 mobile-grid-100 tablet-grid-23')
 listings = []
 
-#* Finding all visible posts and scraping them for price, size, rooms and address
+# * Finding all visible posts and scraping them for price, size, rooms and address
 for ls in posts:
     price = ls.find('div', class_='ar').text[:-4].strip()
     size = ls.find('div', class_='meret').text[7:-3].strip()
-    rooms = ls.find('div', class_='szobalista').text.replace('\n', '').strip()[:-1]
+    rooms = ls.find('div', class_='szobalista').text.replace(
+        '\n', '').strip()[:-1]
     address = ls.find('div', class_='cim').text.strip()
     link = ls.find('a', class_='ingatlan_link')
     listings.append([price, size, rooms, address, link['href']])
 
-#* Finding all hidden posts and scraping them for price, size, rooms and address
-hidden = soup.find_all('div', class_='ingatlan-cont grid-23 mobile-grid-100 tablet-grid-23 invisible')
+# * Finding all hidden posts and scraping them for price, size, rooms and address
+hidden = soup.find_all(
+    'div', class_='ingatlan-cont grid-23 mobile-grid-100 tablet-grid-23 invisible')
 for ls in hidden:
     price = ls.find('div', class_='ar').text[:-4].strip()
     size = ls.find('div', class_='meret').text[7:-3].strip()
-    rooms = ls.find('div', class_='szobalista').text.replace('\n', '').strip()[:-1]
+    rooms = ls.find('div', class_='szobalista').text.replace(
+        '\n', '').strip()[:-1]
     address = ls.find('div', class_='cim').text.strip()
     link = ls.find('a', class_='ingatlan_link')
     listings.append([price, size, rooms, address, link['href']])
 # driver.close()
 
-#* Going to a new website
+# * Going to a new website
 driver.execute_script("window.open('about:blank', 'secondtab');")
 driver.switch_to.window("secondtab")
 driver.get('https://greatforest.hu/properties?f-business=rent&f-location=&f-room=&f-price-min=&f-price-max=&f-size-min=&f-size-max=&f-exclusive=&f-tag-catalogid=&f-tag-description=&f-keyword=&price-type=eft')
 time.sleep(1)
 
-#* Going through the different pages (1 to 9)
+# * Going through the different pages (1 to 9)
 for i in range(1, 4):
     page2 = driver.page_source
     soup = BeautifulSoup(page2, 'html.parser')
     posts2 = soup.find_all('article', class_='hentry')
-    #* Finding all visible posts and scraping them for price, size, rooms and address
+    # * Finding all visible posts and scraping them for price, size, rooms and address
     for ls in posts2:
-        price = ls.find('span', attrs={'style': 'top: 3px; font-size:16px; text-align: left;'})
+        price = ls.find(
+            'span', attrs={'style': 'top: 3px; font-size:16px; text-align: left;'})
         if price:
             price = price.text[:-3].strip()
         else:
             price = None
-        
+
         size = ls.find('div', class_='size')
         if size:
-            size = size.text[:-3].replace('\n','').strip()
+            size = size.text[:-3].replace('\n', '').strip()
         else:
             size = None
 
@@ -69,30 +75,32 @@ for i in range(1, 4):
             rooms = rooms.text[7:].strip()
         else:
             rooms = None
-        
+
         link = ls.find('a', attrs={'target': '_blank'})
         if link:
             link_new = f'https://www.greatforest.hu{link["href"]}'
         else:
             link_new = None
         listings.append([price, size, rooms, None, link_new])
-    driver.find_element(By.XPATH, f'//*[@id="main-content"]/div[2]/div/div/div[1]/div/div[1]/div[2]/nav/ul/nav/ul/li[{2+i}]/a').click()
-        
+    driver.find_element(
+        By.XPATH, f'//*[@id="main-content"]/div[2]/div/div/div[1]/div/div[1]/div[2]/nav/ul/nav/ul/li[{2+i}]/a').click()
+
 for i in range(6):
     page2 = driver.page_source
     soup = BeautifulSoup(page2, 'html.parser')
     posts2 = soup.find_all('article', class_='hentry')
-    #* Finding all visible posts and scraping them for price, size, rooms and address
+    # * Finding all visible posts and scraping them for price, size, rooms and address
     for ls in posts2:
-        price = ls.find('span', attrs={'style': 'top: 3px; font-size:16px; text-align: left;'})
+        price = ls.find(
+            'span', attrs={'style': 'top: 3px; font-size:16px; text-align: left;'})
         if price:
             price = price.text[:-3].strip()
         else:
             price = None
-        
+
         size = ls.find('div', class_='size')
         if size:
-            size = size.text[:-3].replace('\n','').strip()
+            size = size.text[:-3].replace('\n', '').strip()
         else:
             size = None
 
@@ -101,17 +109,18 @@ for i in range(6):
             rooms = rooms.text[7:].strip()
         else:
             rooms = None
-        
+
         link = ls.find('a', attrs={'target': '_blank'})
         if link:
             link_new = f'https://www.greatforest.hu{link["href"]}'
         else:
             link_new = None
         listings.append([price, size, rooms, None, link_new])
-    driver.find_element(By.XPATH, f'//*[@id="main-content"]/div[2]/div/div/div[1]/div/div[1]/div[2]/nav/ul/nav/ul/li[5]/a').click()
+    driver.find_element(
+        By.XPATH, f'//*[@id="main-content"]/div[2]/div/div/div[1]/div/div[1]/div[2]/nav/ul/nav/ul/li[5]/a').click()
 
 
-#* Going to a new website
+# * Going to a new website
 driver.execute_script("window.open('about:blank', 'thirdtab');")
 driver.switch_to.window("thirdtab")
 driver.get('https://debrecenrent.hu/')
@@ -170,15 +179,12 @@ for ls in posts:
         continue
 
 
-#* Creating a dataframe to store the data in a better format
-df = pd.DataFrame(listings, columns=['Price (HUF)', 'Size', 'Rooms', 'Address', 'Link'])
+# * Creating a dataframe to store the data in a better format
+df = pd.DataFrame(listings, columns=[
+                  'Price (HUF)', 'Size', 'Rooms', 'Address', 'Link'])
 print(len(df))
 
-#* Exporting to an excel file
-df.to_excel('housesTRIAL.xlsx', index=False, header=['Price (HUF)', 'Size', 'Rooms', 'Address', 'Link'])
+# * Exporting to an excel file
+df.to_excel('housesTRIAL.xlsx', index=False, header=[
+            'Price (HUF)', 'Size', 'Rooms', 'Address', 'Link'])
 driver.quit()
-
-
-
-
-
