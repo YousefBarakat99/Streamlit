914869a3c8d3b46fa8231dddb687d6b5f7dc0239
Yousef Barakat <yousefhany19992@gmail.com>
2023-03-19 21:56:42 +0100

    up to date data

--- streamlit/Streamlit_App.py
+++ streamlit/Streamlit_App.py
@@ -357,7 +357,8 @@ def complete():
         y_pred = model.predict(X_test)
         score = r2_score(y_test, y_pred)
         st.write(
-            f'Accuracy of this model is {100 * score:.1f} %. An updated model is under progress and will be posted when ready!')
+            f'Accuracy of this machine learning model is {100 * score:.1f} %. An updated model is under progress and will be posted when ready!')
+        st.write('Enter a size below and the machine learning algorithm will predict how many rooms could fit in the property. The answer is based on all the data gathered previously.')
         new_size = st.number_input('Please enter your desired size: ')
         if new_size > 0:
             pred_rooms = model.predict([[new_size]])
