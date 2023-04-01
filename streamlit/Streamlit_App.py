e131b0c21bba66ea67afea5988deb7f74e9752da
Yousef Barakat <yousefhany19992@gmail.com>
2023-04-02 00:02:53 +0200

    up to date data

--- streamlit/Streamlit_App.py
+++ streamlit/Streamlit_App.py
@@ -382,6 +382,8 @@ def complete():
             and I just need to update it. Please [contact me](https://yousefbarakat99.github.io/website/#contact) 
             if you face any issues.''')
             st.plotly_chart(fig, use_container_width=True)
+            st.write(
+                '## You can find some charts and visualizations below to help you understand how the data is distributed 📊')
             fig = px.histogram(df1, x='Rooms', color='Rooms')
             fig.update_traces(marker_line_width=2, marker_line_color="black")
             st.plotly_chart(fig, use_container_width=True)
