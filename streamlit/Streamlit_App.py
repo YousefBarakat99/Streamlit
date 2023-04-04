0ea75e447394b4772ae108077a585fff91efb48b
Yousef Barakat <yousefhany19992@gmail.com>
2023-04-04 12:15:51 +0200

    Update Streamlit_App.py

--- streamlit/Streamlit_App.py
+++ streamlit/Streamlit_App.py
@@ -73,7 +73,7 @@ def general():
     count = df['Price (HUF)'].count()
     ravg = int(np.mean(df['Rooms']).round())
     savg = int(np.mean(df['Size (m2)']))
-    pavg = int(np.mean(df['Price (HUF)']))
+    pavg = int(np.mean(df[df['Price (HUF)'] <= 450_000]['Price (HUF)']))
     st.success(f'''There is a total of {count} properties gathered across 3 websites. The average number of rooms is {ravg},
      the average size is {savg} m2 and the average rent price is {pavg} HUF per month.''')
     if platform == 'PC/desktop':
